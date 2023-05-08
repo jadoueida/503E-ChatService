cc8207076e65af6de37f2cf503c4cec4b897c587
Karim Shammas <karimshammas@Karims-MacBook-Pro.local>
2023-05-08 05:47:47 +0300

    5

--- ChatService/Controllers/apiController.cs
+++ ChatService/Controllers/apiController.cs
@@ -33,26 +33,33 @@ public class apiController : ControllerBase
     [Route("profile")]
     public async Task<ActionResult<Profile>> AddProfile(Profile profile)
     {
-        var existingProfile = await _profileService.GetProfile(profile.Username);
-        if (existingProfile != null)
+        using(_logger.BeginScope("{Username}",profile.Username))
         {
-            return Conflict($"A user with username {profile.Username} already exists");
+            var existingProfile = await _profileService.GetProfile(profile.Username);
+            if (existingProfile != null)
+            {
+                return Conflict($"A user with username {profile.Username} already exists");
+            }
+
+            await _profileService.UpdateProfile(profile);
+            return CreatedAtAction(nameof(GetProfile), new { username = profile.Username },
+                profile);
         }
-        await _profileService.UpdateProfile(profile);
-        return CreatedAtAction(nameof(GetProfile), new {username = profile.Username},
-            profile); 
     }
     
     [HttpGet("profile/{username}")]
     public async Task<ActionResult<Profile>> GetProfile(string username)
     {
-        var profile = await _profileService.GetProfile(username);
-        if (profile == null)
+        using(_logger.BeginScope("{Username}",username))
         {
-            return NotFound($"A User with username {username} was not found");
+            var profile = await _profileService.GetProfile(username);
+            if (profile == null)
+            {
+                return NotFound($"A User with username {username} was not found");
+            }
+
+            return Ok(profile);
         }
-            
-        return Ok(profile);
     }
     
     
@@ -62,28 +69,30 @@ public class apiController : ControllerBase
     public async Task<ActionResult<ImageResponse>> UploadImage([FromForm] Image request)
     {
         if (request.File == null || request.File.Length == 0)
-        {
+        { 
             return BadRequest("File cannot be null or empty.");
         }
-        var imageId =  await _imageService.UploadImage(request.File);
-       return new ImageResponse(imageId);
+        var imageId = await _imageService.UploadImage(request.File);
+        return new ImageResponse(imageId);
     }
     
     
     
     
     [HttpGet("images/{id}")]
-    
     public async Task<ActionResult<byte[]>> DownloadImage(string id)
     {
-        var image = await _imageService.GetImageById(id);
-
-        if (image == null)
+        using(_logger.BeginScope("{ImageId}",id))
         {
-            return NotFound("This image does not exist");
-        }
+            var image = await _imageService.GetImageById(id);
 
-        return Ok(image.File);
+            if (image == null)
+            {
+                return NotFound("This image does not exist");
+            }
+
+            return Ok(image.File);
+        }
     }
 
 
@@ -91,91 +100,109 @@ public class apiController : ControllerBase
     [Route("conversations")]
     public async Task<ActionResult<Conversation>> StartConversation(ConversationRequest request)
     {
-        
-        if ((request.Participants == null)||
-            (request.Participants.Count != 2))
+        using(_logger.BeginScope("{Username1},{Username2}",request.Participants[0],request.Participants[1]))
         {
-            return BadRequest("Two Participants are Required");
+            if ((request.Participants == null) ||
+                (request.Participants.Count != 2))
+            {
+                return BadRequest("Two Participants are Required");
+            }
+
+            var existingProfile1 = await _profileService.GetProfile(request.Participants[0]);
+            var existingProfile2 = await _profileService.GetProfile(request.Participants[1]);
+            if ((existingProfile1 == null) || (existingProfile2 == null))
+            {
+                return NotFound("Either one or both of the mentioned participants don't exist");
+            }
+
+            string potentialConversationId1 = request.Participants[0] + "_" + request.Participants[1];
+            string potentialConversationId2 = request.Participants[1] + "_" + request.Participants[0];
+            var existingConversation1 = await _conversationService.GetConversationById(potentialConversationId1);
+            var existingConversation2 = await _conversationService.GetConversationById(potentialConversationId2);
+            if ((existingConversation1 != null) ||
+                (existingConversation2 != null))
+            {
+                return Conflict("The conversation already exists");
+            }
+
+
+
+            // 409 if conversation exists
+
+            Conversation response = await _conversationService.CreateConvo(request);
+
+
+            return CreatedAtAction(nameof(StartConversation), response);
         }
-        
-        var existingProfile1 = await _profileService.GetProfile(request.Participants[0]);
-        var existingProfile2 = await _profileService.GetProfile(request.Participants[1]);
-        if ((existingProfile1 == null) || (existingProfile2 == null))
-        {
-            return NotFound("Either one or both of the mentioned participants don't exist");
-        }
-        string potentialConversationId1 = request.Participants[0] + "_" + request.Participants[1];
-        string potentialConversationId2 = request.Participants[1] + "_" + request.Participants[0];
-        var existingConversation1 = await _conversationService.GetConversationById(potentialConversationId1);
-        var existingConversation2 = await _conversationService.GetConversationById(potentialConversationId2);
-        if ((existingConversation1 != null)||
-            (existingConversation2 != null))
-        {
-            return Conflict("The conversation already exists");
-        }
-
-
-
-        // 409 if conversation exists
-
-        Conversation response = await _conversationService.CreateConvo(request);
-        
-    
-        return CreatedAtAction(nameof(StartConversation), response);
     }
 
     [HttpPost]
     [Route("conversations/{conversationId}/messages")]
     public async Task<ActionResult<MessageResponse>> AddMessage(MessageRequest message, string conversationId)
     {
-        var existingMessage = await _messageService.GetMessage(message.MessageId);
-        if (existingMessage != null)
+        using(_logger.BeginScope("{MessageId},{ConversationId}",message.MessageId,conversationId))
         {
-            return Conflict($"A message with MessageId {message.MessageId} already exists");
-        }
-        
-        var existingConversation = await _conversationService.GetConversationById(conversationId);
-        if (existingConversation == null)
-        {
-            return NotFound("The requested conversation does not exist");
-        }
-        if ((message.SenderUsername != existingConversation.Username1)&&
-            (message.SenderUsername!=existingConversation.Username2))
-        {
-            return BadRequest("The Sender User is not a part of the mentioned conversation exist");
-        }
-        
+            var existingMessage = await _messageService.GetMessage(message.MessageId);
+            if (existingMessage != null)
+            {
+                return Conflict($"A message with MessageId {message.MessageId} already exists");
+            }
 
-        long createdUnixTime =await _messageService.AddMessage(message,conversationId);
-        MessageResponse messageResponse = new MessageResponse(createdUnixTime);
-        return CreatedAtAction(nameof(AddMessage), messageResponse);
+            var existingConversation = await _conversationService.GetConversationById(conversationId);
+            if (existingConversation == null)
+            {
+                return NotFound("The requested conversation does not exist");
+            }
 
+            if ((message.SenderUsername != existingConversation.Username1) &&
+                (message.SenderUsername != existingConversation.Username2))
+            {
+                return BadRequest("The Sender User is not a part of the mentioned conversation exist");
+            }
+
+
+            long createdUnixTime = await _messageService.AddMessage(message, conversationId);
+            MessageResponse messageResponse = new MessageResponse(createdUnixTime);
+            return CreatedAtAction(nameof(AddMessage), messageResponse);
+        }
     }
 
     [HttpGet]
     [Route("conversations/{conversationId}/messages")]
     public async Task<ActionResult<MessagesResponse>> GetConvoMessages(string conversationId, string? continuationToken = null, int limit = 50,long lastSeenMessageTime = 0)
     {
-        var existingConversation = await _conversationService.GetConversationById(conversationId);
-        if (existingConversation == null)
+        using(_logger.BeginScope("{ConversationId}",conversationId))
         {
-            return NotFound("The requested conversation does not exist");
+            var existingConversation = await _conversationService.GetConversationById(conversationId);
+            if (existingConversation == null)
+            {
+                return NotFound("The requested conversation does not exist");
+            }
+
+            var messages =
+                await _messageService.GetConversationMessages(conversationId, continuationToken, limit,
+                    lastSeenMessageTime);
+            return Ok(messages);
         }
-        var messages = await _messageService.GetConversationMessages(conversationId, continuationToken, limit,lastSeenMessageTime);
-        return Ok(messages);
     }
     
     [HttpGet]
     [Route("conversations")]
     public async Task<ActionResult<ConversationsResponse>> GetConversations(string username, string? continuationToken = null, int limit=50, long lastSeenConversationTime=0)
     {
-        var existingUser = await _profileService.GetProfile(username);
-        if (existingUser == null)
+        using(_logger.BeginScope("{Username}",username))
         {
-            return NotFound("The user of the requested conversations does not exist");
+            var existingUser = await _profileService.GetProfile(username);
+            if (existingUser == null)
+            {
+                return NotFound("The user of the requested conversations does not exist");
+            }
+
+            var conversations =
+                await _conversationService.GetConversations(username, continuationToken, limit,
+                    lastSeenConversationTime);
+            return Ok(conversations);
         }
-        var conversations = await _conversationService.GetConversations(username, continuationToken, limit, lastSeenConversationTime);
-        return Ok(conversations);
         
     }
 
--- ChatService/DTOs/Profile.cs
+++ ChatService/DTOs/Profile.cs
@@ -1,11 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
 namespace ChatService.DTOs;
 
 public record Profile(
     [Required] string Username, 
     [Required] string FirstName, 
     [Required] string LastName,
-    string ProfilePictureId
+    string? ProfilePictureId
     );
     
     
